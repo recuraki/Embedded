f05f32ff5e74783dfc7dd0456e79b56cecd1aa80
Akira KANAI <glenda.kanai@gmail.com>
2014-01-04 13:24:41 +0900

    コントロール用コードを書いた

---
+++ rasberry-pi/i2cserver.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask, request, url_for
+import os
+
+header = """
+<html><head><title>RasberryPi i2c control</title></head>
+<body>
+"""
+
+footer = """
+</body></html>
+"""
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    c = header
+    c+= "<h1>RasPI i2c Server</h1>"
+    c+= footer 
+    return(c, 200)
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    print(request.args)
+    name = request.args.get("name", "")
+    print("<br>\n")
+    c = ""
+    c += "<hr>"
+    c += "hello" + name
+    c += "<hr>"
+    return(c, 200)
+
+@app.route('/set', methods=['GET'])
+def hello_withname():
+    bus = request.args.get("name", "")
+    value = request.args.get("num", "")
+    if bus == "" or value == "":
+        return("<font color=red>need bus and value</font>", 200)
+    cmd = "i2cset 1 0x{0} {1}".format(bus, value)
+    res = "hoge"
+    c = header
+    c+= "<h1>exec mode</h1>"
+    c+= "exec: {0} <br>".format(cmd)
+    c+= "result: {0} <br>".format(res)
+    c+= footer 
+    return(c, 200)
+
+with app.test_request_context():
+    #print url_for('hello')
+    #print url_for('set')
+
+if __name__ == '__main__':
+
+    app.run(debug=True, host="0.0.0.0")
